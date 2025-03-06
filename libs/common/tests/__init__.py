aa073cdfb6813dca6d6b51d6829c7011f3f653a2
Josejulio Martínez <jmartine@redhat.com>
2025-03-06 10:55:17 -0700

    lint fix

--- libs/common/tests/__init__.py
+++ libs/common/tests/__init__.py
@@ -6,6 +6,7 @@ _base = pathlib.Path(__file__).parent.resolve().joinpath("resources")
 def path_to_resource(resource):
     return str(_base.joinpath(resource))
 
+
 def get_resource_contents(resource):
     path = path_to_resource(resource)
     buff = []
