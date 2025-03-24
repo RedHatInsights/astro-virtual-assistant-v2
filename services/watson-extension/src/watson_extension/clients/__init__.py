1ba2bdf9e6a5dbaee18147dd2a985ee8522b9fd7
Justin Orringer <justinorringer@gmail.com>
2025-03-24 15:57:29 -0400

    Cleanup duplicate RHSMUrl

--- services/watson-extension/src/watson_extension/clients/__init__.py
+++ services/watson-extension/src/watson_extension/clients/__init__.py
@@ -1,7 +1,6 @@
 import typing
 
 AdvisorURL = typing.NewType("AdvisorURL", str)
-RHSMURL = typing.NewType("RHSMURL", str)
 VulnerabilityURL = typing.NewType("VulnerabilityURL", str)
 ContentSourcesURL = typing.NewType("ContentSourcesURL", str)
 RhsmURL = typing.NewType("RhsmURL", str)
