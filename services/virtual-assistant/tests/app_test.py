89ab4c89e9d5ce2aab4b558822c2250c8b82663c
Josejulio Martínez <josejulio@users.noreply.github.com>
2025-04-02 11:48:16 -0600

    Test injector configuration (#65)
    - This tests that we have injected everything for a
       correct startup - it will at least check that all
       the route injectors have an option

--- services/virtual-assistant/tests/app_test.py
+++ services/virtual-assistant/tests/app_test.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import quart_injector
+import injector
+import aiohttp
 
 from unittest import mock
 from unittest.mock import MagicMock
@@ -62,7 +65,8 @@ async def assistant_v2():
     return mocked
 
 
-async def test_app(assistant_v2, redis_fixture, aiohttp_mock, clear_app_config):
+@pytest.fixture
+async def default_app(redis_fixture, assistant_v2):
     redis_credentials = redis_fixture.pmr_credentials
     with (
         mock.patch("ibm_watson.AssistantV2", MagicMock(return_value=assistant_v2)),
@@ -89,48 +93,79 @@ async def test_app(assistant_v2, redis_fixture, aiohttp_mock, clear_app_config):
             clear=True,
         ),
     ):
-        aiohttp_mock.post(
-            "rhel-lightspeed/api/lightspeed/v1/infer",
-            status=200,
-            body=RhelLightspeedResponse(
-                data=RhelLightspeedData(
-                    model_id="a-model",
-                    text="42",
-                )
-            ).model_dump_json(),
-        )
-
         from run import app
 
-        test_client = app.test_client()
-        response = await test_client.post(
-            "/api/virtual-assistant/v2/talk",
-            json={
-                "session_id": None,
-                "input": {
-                    "text": "what's the answer to the ultimate question of life, the universe, and everything?",
-                },
-            },
-            headers={
-                "x-rh-identity": "eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiJhY2NvdW50MTIzIiwib3JnX2lkIjoib3JnMTIzIiwidHlwZSI6IlVzZXIiLCJ1c2VyIjp7ImlzX29yZ19hZG1pbiI6dHJ1ZSwgInVzZXJfaWQiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJhc3RybyJ9LCJpbnRlcm5hbCI6eyJvcmdfaWQiOiJvcmcxMjMifX19",
-            },
-        )
+        yield app
+        # Close the ClientSession to prevent some warnings
+        injector_container = app.extensions["injector"]
+        assert injector_container is not None
+        session: aiohttp.ClientSession = injector_container.get(aiohttp.ClientSession)
+        await session.close()
 
-        assert response.status == "200 OK"
-        json = await response.get_json()
-        talk_response = TalkResponse(**json)
 
-        assert talk_response is not None
-        assert talk_response.session_id == "session_id_0001"
-        assert len(talk_response.response) == 3
-        assert talk_response.response[0].type == ResponseType.TEXT
-        assert talk_response.response[0].text == "hello world"
-        assert talk_response.response[1].type == ResponseType.TEXT
-        assert talk_response.response[1].text == "42"
-        assert talk_response.response[2].type == ResponseType.OPTIONS
-        assert talk_response.response[2].text == "Do you want pie?"
-        assert len(talk_response.response[2].options) == 2
-        assert talk_response.response[2].options[0].text == "Bring it on!"
-        assert talk_response.response[2].options[0].value == "yes"
-        assert talk_response.response[2].options[1].text == "Eww!"
-        assert talk_response.response[2].options[1].value == "no"
+async def test_app_injection(default_app):
+    injector_container = default_app.extensions["injector"]
+    assert injector_container is not None
+
+    # Manually inject the RequestScope - since we are not going to do any request
+    injector_container.get(quart_injector.RequestScope).push()
+
+    for rule in default_app.url_map.iter_rules():
+        if rule.endpoint not in default_app.view_functions:
+            continue
+
+        if rule.endpoint == "openapi":
+            continue
+
+        route_function = default_app.view_functions.get(rule.endpoint)
+        bindings = injector.get_bindings(route_function)
+
+        for param in bindings.values():
+            if injector_container.get(param) is None:
+                assert injector_container.create_object(param) is not None
+
+
+async def test_app(default_app, aiohttp_mock):
+    test_client = default_app.test_client()
+    aiohttp_mock.post(
+        "rhel-lightspeed/api/lightspeed/v1/infer",
+        status=200,
+        body=RhelLightspeedResponse(
+            data=RhelLightspeedData(
+                model_id="a-model",
+                text="42",
+            )
+        ).model_dump_json(),
+    )
+
+    response = await test_client.post(
+        "/api/virtual-assistant/v2/talk",
+        json={
+            "session_id": None,
+            "input": {
+                "text": "what's the answer to the ultimate question of life, the universe, and everything?",
+            },
+        },
+        headers={
+            "x-rh-identity": "eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiJhY2NvdW50MTIzIiwib3JnX2lkIjoib3JnMTIzIiwidHlwZSI6IlVzZXIiLCJ1c2VyIjp7ImlzX29yZ19hZG1pbiI6dHJ1ZSwgInVzZXJfaWQiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJhc3RybyJ9LCJpbnRlcm5hbCI6eyJvcmdfaWQiOiJvcmcxMjMifX19",
+        },
+    )
+
+    assert response.status == "200 OK"
+    json = await response.get_json()
+    talk_response = TalkResponse(**json)
+
+    assert talk_response is not None
+    assert talk_response.session_id == "session_id_0001"
+    assert len(talk_response.response) == 3
+    assert talk_response.response[0].type == ResponseType.TEXT
+    assert talk_response.response[0].text == "hello world"
+    assert talk_response.response[1].type == ResponseType.TEXT
+    assert talk_response.response[1].text == "42"
+    assert talk_response.response[2].type == ResponseType.OPTIONS
+    assert talk_response.response[2].text == "Do you want pie?"
+    assert len(talk_response.response[2].options) == 2
+    assert talk_response.response[2].options[0].text == "Bring it on!"
+    assert talk_response.response[2].options[0].value == "yes"
+    assert talk_response.response[2].options[1].text == "Eww!"
+    assert talk_response.response[2].options[1].value == "no"
