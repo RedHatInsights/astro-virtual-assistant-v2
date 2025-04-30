b0b7d433867f8881e871fa9dad72e79b675ebeda
Justin Orringer <justinorringer@gmail.com>
2025-04-30 13:50:40 -0400

    Adding RBAC back to routes list

--- services/watson-extension/src/watson_extension/routes/platform/__init__.py
+++ services/watson-extension/src/watson_extension/routes/platform/__init__.py
@@ -1,8 +1,9 @@
 from quart import Blueprint
-from . import chrome, notifications, integrations
+from . import chrome, rbac, notifications, integrations
 
 blueprint = Blueprint("platform", __name__, url_prefix="/platform")
 
 blueprint.register_blueprint(chrome.blueprint)
 blueprint.register_blueprint(notifications.blueprint)
 blueprint.register_blueprint(integrations.blueprint)
+blueprint.register_blueprint(rbac.blueprint)
