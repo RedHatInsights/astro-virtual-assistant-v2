903a012d66e4379d42df88fa249b121582e8a54a
Justin Orringer <justinorringer@gmail.com>
2025-05-05 16:52:16 -0400

    Removing use of platform_request in the services offline client (#124)
    * Removing use of platform_request in the client
    
    * Using logger and raising exception

--- services/watson-extension/src/watson_extension/clients/general/redhat_status.py
+++ services/watson-extension/src/watson_extension/clients/general/redhat_status.py
@@ -1,8 +1,6 @@
 import abc
-import injector
 import logging
-from aiohttp import ClientResponse
-from watson_extension.clients.platform_request import AbstractPlatformRequest
+from aiohttp import ClientResponse, ClientSession
 
 
 logger = logging.getLogger(__name__)
@@ -16,12 +14,19 @@ class RedhatStatusClient(abc.ABC):
 class RedhatStatusClientHttp(RedhatStatusClient):
     def __init__(
         self,
-        platform_request: injector.Inject[AbstractPlatformRequest],
     ):
         super().__init__()
-        self.platform_request = platform_request
 
     async def check_services_offline(self) -> ClientResponse:
-        base_url = "https://status.redhat.com"
-        request = "/api/v2/incidents/unresolved.json"
-        return await self.platform_request.get(base_url, request)
+        try:
+            async with ClientSession() as session:
+                async with session.get(
+                    "https://status.redhat.com/api/v2/incidents/unresolved.json"
+                ) as status_response:
+                    result = await status_response.json()
+        except Exception as e:
+            logger.error(
+                f"An Exception occured while handling response from status.redhat.com: {e}"
+            )
+            raise e
+        return result
